aa9550f5b862056028c009ab5a39017fd93e8fa7
Keaty Gross <kgross@pivotal.io>
2017-08-09 17:18:15 -0400

    Add integration test suite helpers
    Signed-off-by: Sam Coward <scoward@pivotal.io>

---
+++ cutlass/test_helpers.go
@@ -0,0 +1,85 @@
+package cutlass
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/cloudfoundry/libbuildpack"
+	"github.com/cloudfoundry/libbuildpack/packager"
+)
+
+type VersionedBuildpackPackage struct {
+	Version string
+	File    string
+}
+
+func FindRoot() (string, error) {
+	file := "VERSION"
+	for {
+		files, err := filepath.Glob(file)
+		if err != nil {
+			return err
+		}
+		if len(files) == 1 {
+			file, err = filepath.Abs(filepath.Dir(file))
+			if err != nil {
+				return err
+			}
+			return file
+		}
+		file = filepath.Join("..", file)
+	}
+}
+
+func PackageUniquelyVersionedBuildpack() (PackagedBuildpack, error) {
+	bpDir, err := FindRoot()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(filepath.Join(bpDir, "VERSION"))
+	if err != nil {
+		return nil, err
+	}
+	buildpackVersion = string(data)
+	buildpackVersion = fmt.Sprintf("%s.%s", buildpackVersion, time.Now().Format("20060102150405"))
+
+	file, err := packager.Package(bpDir, packager.CacheDir, buildpackVersion, cutlass.Cached)
+	if err != nil {
+		return nil, err
+	}
+
+	var manifest struct {
+		Language string `yaml:"language"`
+	}
+	err := libbuildpack.NewYAML().Load(filepath.Join(bpDir, "manifest.yml"), &manifest)
+	if err != nil {
+		return nil, err
+	}
+
+	err := UpdateBuildpack(manifest.Language, file)
+	if err != nil {
+		return nil, err
+	}
+
+	return PackagedBuildpack{
+		Version: buildpackVersion,
+		File:    file,
+	}, nil
+}
+
+func SeedRandom() {
+	seed := int64(time.Now().Nanosecond() + os.Getpid())
+	rand.Seed(seed)
+}
+
+func RemovePackagedBuildpack(buildpack VersionedBuildpackPackage) error {
+	err := os.Remove(buildpack.File)
+	if err != nil {
+		return err
+	}
+}
